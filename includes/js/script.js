3f6fe646838f73a9780bf4ce69578d12344f978b
imadx <ahtimadhusanka@gmail.com>
2015-12-09 11:06:51 +0530

    updates from Isuru

---
+++ includes/js/script.js
@@ -0,0 +1,43 @@
+$(document).click(function () {
+    setSize();
+});
+
+
+function setSize() {
+    var body = $("body").height();
+    var win = $(window).height();
+    var win_width = $(document).width();
+    if (body < win) {
+        $("body").height(win);
+        $("#main").height(win - $("#footerContainer").height() - $("#myCarousel").height());
+        $("#indexDetails").height(body - $("#footerContainer").height() - $("#myCarousel").height() + 180);
+    } else {
+        $("body").css('height', 'auto');
+        $("#mainBody").css('height', 'auto');
+        $("#footerContainer").css('position', 'relative');
+
+    }
+}
+
+$(document).ready(function () {
+    setSize();
+});
+
+
+
+$(window).on('resize', function () {
+    setSize();
+    //alert($(window).width());
+    if ($(window).width() < 750) {
+        $("#mainBody").css('background-color', 'black');
+        $("body").css('height', 'auto');
+        $("#mainBody").css('height', 'auto');
+        $("#indexDetails").css('height', 'auto');
+    } else {
+
+        $("#mainBody").css('background-color', '');
+
+    }
+
+
+});
